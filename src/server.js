99e6b203eb73228b0f91237715827304ca1f7809
crsa <cristianosalvatore.anselmi@betssongroup.com>
2018-01-26 00:30:50 +0100

    feat: use cors

--- src/server.js
+++ src/server.js
@@ -1,23 +1,23 @@
 import express from 'express';
 import { apolloExpress, graphiqlExpress } from 'apollo-server';
 import rootSchema from './schema/rootSchema';
+import cors from 'cors';
 
 const bodyParser = require('body-parser');
 
 const GRAPHQL_PORT = 8000;
 
 const app = express();
+app.use(cors());
 
 app.use('/graphql', bodyParser.json(), apolloExpress({
   schema: rootSchema,
 }));
 
-if (process.env.NODE_ENV === 'development') {
   app.use('/graphiql', graphiqlExpress({
     endpointURL: '/graphql',
     context: {},
   }));
-}
 
 app.listen(GRAPHQL_PORT, () => {
   /* eslint-disable no-console */
